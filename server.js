ec5c061a01e8401fadc2bd15f742365d5d228601
alex <alex@cloudcoreo.com>
2019-03-16 17:19:36 +0600

    added endpoint for open secret

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
 const mockSecrets = [
     {
@@ -12,6 +13,7 @@ const mockSecrets = [
     }
 ]
  
+app.use(cors())
 
 const findSecret = (secretId) => {
     const secret = mockSecrets.find((secret) => secret.id === secretId);
@@ -23,28 +25,38 @@ const findSecret = (secretId) => {
 
 app.post('/secret', (req, res) => res.status(200).send('Not supported yet'));
 
-app.get('/secret/:secretId', (req, res) => {
+app.get('/secret/:secretId/open', (req, res) => {
     try {
         const secretId = req.params.secretId;
         const passphrase = req.query.passphrase;
-
+        console.log(secretId, passphrase);
         const secret = findSecret(secretId);
-        
         if (secret.passphrase && !passphrase) {
             throw { message: 'passphrase needed', code: 403 }; 
         }
-
         if (secret.passphrase && secret.passphrase != passphrase) {
             throw { message: 'secret not found', code: 404 }; 
         }
-
         return res.status(200).json(secret);
+    } catch(error) {
+        return res.status(error.status || 500).json(error.message || error);
+    }
+});
+
+app.get('/secret/:secretId', (req, res) => {
+    try {
+        const secretId = req.params.secretId;
+        const secret = findSecret(secretId);
+        if (!secret) {
+            throw { message: 'secret not found', code: 404 }; 
+        }
+        return res.status(200).json({ secretId, exists: true });
     } catch (error) {
         return res.status(error.status || 500).json(error.message || error);
     }
 });
 
-app.set('port', 3000);
+app.set('port', 8081);
  
 app.listen(app.get('port'), () => {
     console.log(`server starts at ${app.get('port')}`)
